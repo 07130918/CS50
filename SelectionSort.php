4f74d10f4ae74f21be40e826129367e4d2d2f53d
こた <tokyosato@gmail.com>
2021-10-03 16:42:17 +0900

    SelectionSort

---
+++ SelectionSort.php
@@ -0,0 +1,23 @@
+<?php
+// 計算量O(n^2)
+function selectSort($numbers)
+{
+    for ($i = 0; $i < count($numbers) - 1; $i++) {
+        $min = $numbers[$i];
+        $num_index = $i;
+        for ($j = $num_index++; $j < count($numbers); $j++) {
+            if ($min > $numbers[$j]) {
+                $num_index = $j;
+                $min = $numbers[$j];
+            }
+        }
+        $numbers[$num_index] = $numbers[$i];
+        $numbers[$i] = $min;
+    }
+    return $numbers;
+}
+
+$numbers = [2, 9, 8, 5, 6, 1, 3, 4, 10, 7];
+echo join(",", selectSort($numbers));
+echo PHP_EOL;
+?>
