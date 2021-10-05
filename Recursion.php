208429d6b92995c71f57939a63b2ad67cb2c9f92
こた <tokyosato@gmail.com>
2021-10-05 16:17:17 +0900

    階上を再帰を利用し解く

---
+++ Recursion.php
@@ -0,0 +1,24 @@
+<?php
+// 再帰関数
+function fact(int $n):int {
+    if ($n === 0) return 1;
+
+    if ($n === 1) {
+        return 1;
+    } else {
+        return $n * fact($n - 1);
+    }
+}
+
+function fact2(int $n):int {
+    $product = 1;
+    while($n > 0) {
+        $product *= $n;
+        $n--;
+    }
+    return $product;
+}
+
+echo fact(5), PHP_EOL;
+echo fact2(4), PHP_EOL;
+?>
