11932cba3309dd94288f16514959a444bc73a304
こた <tokyosato@gmail.com>
2021-10-10 15:41:08 +0900

    quicksort

---
+++ Algorithms/QuickSort.php
@@ -0,0 +1,27 @@
+<?php
+// 計算量O(), Ω()
+// 基準値を決めてそれより大きいか小さいかを繰り返す
+
+function quick_sort(array $array): array {
+    if (count($array) <= 1) return $array;
+
+    // 基準点(ピボット)を決める
+    $pivot = array_shift($array);
+    $left = [];
+    $right = [];
+
+    for ($i=0; $i < count($array); $i++) {
+        // ピボットより小さい数は左, 大きい数は右
+        if ($array[$i] < $pivot) {
+            $left[]  = $array[$i];
+        } else {
+            $right[] = $array[$i];
+        }
+    }
+    // 左右のデータを再帰的にソートする
+    return array_merge(quick_sort($left), [$pivot], quick_sort($right));
+}
+
+$unsorted_array = [5, 2, 1, 3, 6, 4];
+echo join(",", quick_sort($unsorted_array)), PHP_EOL;
+?>
