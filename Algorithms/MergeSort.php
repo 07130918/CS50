6c2cc9978d3db644fd35f9b897be359c5679dadb
こた <tokyosato@gmail.com>
2021-10-10 14:32:12 +0900

    コメント追記

--- Algorithms/MergeSort.php
+++ Algorithms/MergeSort.php
@@ -3,9 +3,10 @@
 
 function merge_sort(array $my_array)
 {
+    // 単一の要素が来たらそのまま返せば良い(単一要素はsort済み)
     if (count($my_array) === 1) return $my_array;
 
-    // 配列を前半部分と後半部分に分割
+    // 配列を前半部分と後半部分に分割, (int)は小数以下切り捨て
     $mid = (int)(count($my_array) / 2);
     $left = array_slice($my_array, 0, $mid);
     $right = array_slice($my_array, $mid);
@@ -36,12 +37,14 @@ function merge(array $left, array $right)
     // どちらかの配列が空になった時
     while(!empty($left)) // $rightが空
     {
+        // echo "rightなし", PHP_EOL;
         $merged_array[] = $left[0];
         $left = array_slice($left, 1);
     }
 
     while(!empty($right)) // $leftが空
     {
+        // echo "leftなし", PHP_EOL;
         $merged_array[] = $right[0];
         $right = array_slice($right, 1);
     }
@@ -50,6 +53,7 @@ function merge(array $left, array $right)
     return $merged_array;
 }
 
-$unsorted_array = [3, 2, 1, 4, 15, 18, 13, 99, 77, 66, 100, 0];
+// $unsorted_array = [3, 2, 1, 4, 15, 18, 13, 99, 77, 66, 100, 0];
+$unsorted_array = [5, 2, 1, 3, 6, 4];
 echo join(",", merge_sort($unsorted_array)), PHP_EOL;
 ?>
