0891ad388474eade62953bb195d491a10dc0cfb8
こた <tokyosato@gmail.com>
2021-10-10 10:27:46 +0900

    guardの統合

--- Algorithms/Recursion.php
+++ Algorithms/Recursion.php
@@ -1,13 +1,9 @@
 <?php
 // 再帰関数
 function fact(int $n):int {
-    if ($n === 0) return 1;
+    if ($n === 0 || $n === 1) return 1;
 
-    if ($n === 1) {
-        return 1;
-    } else {
-        return $n * fact($n - 1);
-    }
+    return $n * fact($n - 1);
 }
 
 function fact2(int $n):int {
