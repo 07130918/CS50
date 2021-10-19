a04554afe2d522ba01696c2e9817e7178ef90625
こた <tokyosato@gmail.com>
2021-10-19 11:08:39 +0900

    2分探索に再帰関数を追加

--- Algorithms/BinarySearch.php
+++ Algorithms/BinarySearch.php
@@ -26,8 +26,35 @@ function binary_search($numbers, $target) {
 
 $numbers = range(1, 100);
 $target = 100;
-$anser = binary_search($numbers, $target);
+$answer = binary_search($numbers, $target);
 
-echo "ターゲット:{$anser[0]}", PHP_EOL;
-echo "試行回数:{$anser[1]}", PHP_EOL;
+echo "binary_search execution", PHP_EOL;
+echo "ターゲット:{$answer[0]}", PHP_EOL;
+echo "試行回数:{$answer[1]}", PHP_EOL;
+
+
+function binary_search_recursion(array $numbers, int $target) {
+    // ターゲットに配列最大値より大きい自然数が来た時ガード
+    if (count($numbers) === 1 && $numbers[0] != $target) return 1;
+
+    $half = count($numbers) / 2;
+
+    if ($numbers == null) {
+        return 1;
+    } elseif ($target < $numbers[$half]) {
+        return binary_search_recursion(array_slice($numbers, 0, $half), $target);
+    } elseif ($target > $numbers[$half]) {
+        return binary_search_recursion(array_slice($numbers, $half), $target);
+    } elseif ($target === $numbers[$half]) {
+        return 0;
+    }
+}
+
+$target = 1;
+$answer = binary_search_recursion(range(1,10), $target);
+
+echo "binary_search_recursion execution", PHP_EOL;
+echo "ターゲット:{$target}", PHP_EOL;
+echo "Response statement:{$answer}", PHP_EOL;
+
 ?>
