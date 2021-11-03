3ca8f19b6704c3a5ac993f9d4bd387225fb97b82
こた <tokyosato1@gmail.com>
2021-11-03 15:12:32 +0900

    DNA塩基配列から個人を特定するプログラム

---
+++ Lab_ProblemSet/DNA/dna.py
@@ -0,0 +1,51 @@
+import csv
+import sys
+
+
+def main(dna_database, text_file):
+    sequence = get_sequence(text_file)
+    short_tandem_repeats = get_short_tandem_repeats(dna_database)
+    dna_pattern = create_dna_pattern_dict(short_tandem_repeats, sequence)
+
+    print(search_person(dna_database, dna_pattern))
+
+
+def get_sequence(file):
+    with open(file, "r") as f:
+        return f.read().replace('\n', '')
+
+
+def get_short_tandem_repeats(dna_database):
+    with open(dna_database, "r") as file:
+        short_tandem_repeats_list = file.readlines()[0].replace('\n', '').split(',')
+        short_tandem_repeats_list.pop(0)
+    return short_tandem_repeats_list
+
+
+def create_dna_pattern_dict(short_tandem_repeats, sequence):
+    dna_pattern_dict = {}
+    for i in short_tandem_repeats:
+        dna_pattern_dict[i] = str(get_repeat_count(i, sequence.count(i), sequence))
+    return dna_pattern_dict
+
+
+def get_repeat_count(i, repeat_count, sequence):
+    """DNA配列(i)がsequence文字列に連続で出現する最大回数を求める再帰関数"""
+    if i * repeat_count in sequence:
+        return repeat_count
+
+    return get_repeat_count(i, repeat_count - 1, sequence)
+
+
+def search_person(dna_database, dna_pattern):
+    with open(dna_database, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            name = row.pop('name')
+            if row == dna_pattern:
+                return name
+    return 'No match'
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
