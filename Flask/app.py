551a44db4ec4a049606c64b8488467933680ca31
こた <tokyosato1@gmail.com>
2021-11-30 17:40:17 +0900

    branch response view and create gurde if syntax

--- Flask/app.py
+++ Flask/app.py
@@ -1,19 +1,30 @@
-from crypt import methods
 from flask import Flask
 from flask import render_template
 from flask import request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "Dodgeball",
+    "Flag Football",
+    "Soccer",
+    "Volleyball",
+    "Ultimate Frisbee"
+]
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 
 @app.route("/register", methods=["POST"])
 def register():
+    # 不正な値をガード
     # request.argsでhttpのget, .formでpost
-    # 第2引数にデフォルト値を取れる
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
+        return render_template("failure.html")
+    
+    # render_templateは値の引き渡しができる. get()の第2引数にデフォルト値を取れる
     # return render_template("hoge.html", name=request.form.get("name", "world"))
     return render_template("success.html")
