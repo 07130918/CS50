14ad9f40a9ef3b2fe7f2d809439d600892a60555
こた <tokyosato1@gmail.com>
2021-12-02 12:31:31 +0900

    :rocket: Flask!!!

--- Flask/app.py
+++ Flask/app.py
@@ -15,6 +15,8 @@ SPORTS = [
 
 @app.route("/")
 def index():
+    # render_templateは値の引き渡しができる. フォームから値が送られてくる場合, get()の第2引数にデフォルト値を取れる
+    # return render_template("hoge.html", name=request.form.get("name", "world"))
     return render_template("index.html", sports=SPORTS)
 
 
@@ -22,9 +24,13 @@ def index():
 def register():
     # 不正な値をガード
     # request.argsでhttpのget, .formでpost
-    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
-        return render_template("failure.html")
-    
-    # render_templateは値の引き渡しができる. get()の第2引数にデフォルト値を取れる
-    # return render_template("hoge.html", name=request.form.get("name", "world"))
-    return render_template("success.html")
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    if not name:
+        return render_template("error.html", message="Missing name")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+
+    return render_template("/success.html")
