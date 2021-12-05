e00e44ea7ec495096f2902117826c20726a5650d
こた <tokyosato1@gmail.com>
2021-12-05 16:00:52 +0900

    form未完

--- Lab_ProblemSet/Lab9/app.py
+++ Lab_ProblemSet/Lab9/app.py
@@ -1,19 +1,18 @@
-import os
 import sqlite3
-from flask import g
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+from flask import (Flask, flash, g, jsonify, redirect, render_template,
+                   request, session)
 
 # Configure application
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
+# https://msiz07-flask-docs-ja.readthedocs.io/ja/latest/patterns/sqlite3.html
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect('birthdays.db')
+        db.row_factory = dict_factory
     return db
 
 
@@ -27,6 +26,16 @@ def close_connection(exception):
         db.close()
 
 
+def dict_factory(cursor, row):
+    """
+        取得レコードをtuple型からdict型に変換
+    """
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     db = get_db()
@@ -34,11 +43,21 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        print(type(month), month)
+        curs.execute(
+            'INSERT INTO birthdays(name, month, day)'
+            f'values("{name}", "{month}, {day}")'
+        )
+        db.commit()
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
-
-        return render_template("index.html")
+        curs.execute('SELECT * FROM birthdays')
+        persons = curs.fetchall()
+        print(persons)
+        return render_template("index.html", persons=persons)
