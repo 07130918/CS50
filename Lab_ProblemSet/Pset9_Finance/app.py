4b776a7e17a9560b2630d13122b064980c2a7ea9
こた <tokyosato1@gmail.com>
2021-12-23 13:30:23 +0900

    import delete

--- Lab_ProblemSet/Pset9_Finance/app.py
+++ Lab_ProblemSet/Pset9_Finance/app.py
@@ -5,7 +5,6 @@ import sqlite3
 from flask import Flask, g, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from sympy import quo
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
